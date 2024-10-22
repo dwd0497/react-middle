799c6417455cdf400d733d3ab7db81ed0025aad1
Денисов Павел Валерьевич <PDenisov@alfabank.ru>
2024-10-22 21:57:39 +0500

    * add Navbar and AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
 import "./styles/index.scss"
-import {Link} from "react-router-dom";
-import {classNames} from "shared/lib/helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={classNames('app',{}, [theme])}>
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar/>
             <button onClick={toggleTheme}>Toggle theme</button>
-            <Link to='/'>Main page</Link>
-            <Link to='about'>About page</Link>
-            <AppRouter />
+            <AppRouter/>
         </div>
     );
 };
